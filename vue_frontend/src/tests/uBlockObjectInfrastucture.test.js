cbb977a07af125aa3448c2ae0f7ab624132d587e
Mark Lockett <mark@deliveryengine.net>
2021-11-02 17:24:48 +0800

    Get unit tests working

---
+++ vue_frontend/src/tests/uBlockObjectInfrastucture.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe("verify_we_can_load_files_intended_for_background_page", () => {
+    it("test_using_eval_to_do_an_import", () => {
+        console.log("Hello unit tests");
+        expect(1).toBe(1);
+    });
+});
