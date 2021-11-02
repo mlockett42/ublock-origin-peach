279c6389dee9c10db85f22462495233a7066c3fe
Mark Lockett <mark@deliveryengine.net>
2021-11-02 17:35:28 +0800

    Unit test that simulates the behaviour of the background page

--- vue_frontend/src/tests/uBlockObjectInfrastucture.test.js
+++ vue_frontend/src/tests/uBlockObjectInfrastucture.test.js
@@ -1,8 +1,17 @@
 'use strict';
 
+const fs = require('fs')
+
 describe("verify_we_can_load_files_intended_for_background_page", () => {
     it("test_using_eval_to_do_an_import", () => {
-        console.log("Hello unit tests");
-        expect(1).toBe(1);
+        const data = fs.readFileSync('../src/js/chromeLocalStorageService.js', 'utf8')
+
+        // Simulate what the background does when it loads the file
+        let µBlock = { };
+        eval(data);
+
+        // Verify our files got attached to the µBlock object
+        expect(µBlock.localStorageSet).not.toBeFalsy();
+        expect(µBlock.localStorageGet).not.toBeFalsy();
     });
 });
