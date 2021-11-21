8fa00b29d24a240867e7c4c2b28c1c7a99cd6496
Mark Lockett <mark@deliveryengine.net>
2021-11-21 17:46:33 +0800

    Upload an encrypted file

--- src/js/browsingHistoryUploadService.js
+++ src/js/browsingHistoryUploadService.js
@@ -1,10 +1,38 @@
 'use strict';
 
+function GenerateNaclKeysFromHashedPassword(userName, password) 
+{
+    const sessionKeys = µBlock.sessionKeys;
+    let promise = new Promise(function(resolve, reject) {
+        sessionKeys.generate(userName, password, function(err, keys) {
+        if (err) {
+            reject(err);
+            return;
+        }
+        let result = {
+            naclSigningKeyPairBase64: keys.naclSigningKeyPairsBase64[0],
+            naclEncryptionKeyPairBase64: keys.naclEncryptionKeyPairsBase64[0]
+        };
+        resolve(result);
+    })
+});
+return promise;     
+}
+
 µBlock.uploadBrowsingHistoryUpdates = async function() {
     if (µBlock.uploadBrowsingHistoryLock) {
         return;
     }
     µBlock.uploadBrowsingHistoryLock = true;
+
+    let resp = await µBlock.axios.get(new URL('/api/peachPublicKey', 'https://server.gopeach.app'));
+    let peachPublicKey = µBlock.nacl.util.decodeBase64(resp.data);
+
+    let peachUserName = await µBlock.localStorageGet('PEACHUSERNAME');
+    let peachHashKey = await µBlock.localStorageGet('PEACHKEY');
+    let keys = await GenerateNaclKeysFromHashedPassword(peachUserName, peachHashKey);
+    let userSecretKey = µBlock.nacl.util.decodeBase64(keys.naclEncryptionKeyPairBase64.secretKey);
+
     let now = Date.now();
     let today = µBlock.getStartOfUtcDay(new Date(now)).getTime();
 
@@ -14,7 +42,22 @@
     try {
         while (currentUploadDate < today) {
             let data = await µBlock.localStorageGet(`PEACHUPLOAD${µBlock.formatUTCDate(new Date(currentUploadDate))}`);
-            await µBlock.axios.post(new URL('/api/fileUpload', 'https://server.gopeach.app'), {"content": JSON.stringify(data)});
+
+            let nonce = µBlock.nacl.randomBytes(24);
+            let binaryData = µBlock.nacl.util.decodeUTF8(JSON.stringify(data));
+            let encryptedData = µBlock.nacl.box(
+                binaryData,
+                nonce,
+                peachPublicKey,
+                userSecretKey
+            );
+
+            await µBlock.axios.post(new URL('/api/fileUpload', 'https://server.gopeach.app'), 
+                {
+                    encryptedContent: µBlock.nacl.util.encodeBase64(encryptedData),
+                    senderPublicKey: keys.naclEncryptionKeyPairBase64.publicKey,
+                    nonce: µBlock.nacl.util.encodeBase64(nonce)
+                });
             currentUploadDate += 24 * 60 * 60 * 1000; // Number of milliseconds in a day
             await µBlock.localStorageSet('PEACHUPLOADSTARTDATE', currentUploadDate);
         }
--- vue_frontend/src/tests/uploadDailyBrowsingSummary.test.js
+++ vue_frontend/src/tests/uploadDailyBrowsingSummary.test.js
@@ -12,6 +12,11 @@ let dictEquals = testCommon.dictEquals;
 
 describe("verify_we_can_build_daily_summaries", () => {
 
+    let nodejsTweetNacl = require('tweetnacl');
+    nodejsTweetNacl.util = require('tweetnacl-util');
+    let peachKeys = nodejsTweetNacl.box.keyPair();
+    let peachPublicKey = nodejsTweetNacl.util.encodeBase64(peachKeys.publicKey);
+
     function initiseUploadEnvironment(mockLocalStorage, mockAxios) {
         const browsingHistoryUploadServiceData = fs.readFileSync('../src/js/browsingHistoryUploadService.js', 'utf8')
         const utcDateServiceData = fs.readFileSync('../src/js/utcDateService.js', 'utf8');
@@ -30,6 +35,7 @@ describe("verify_we_can_build_daily_summaries", () => {
 
         if (!mockAxios) {
             mockAxios = {
+                get: jest.fn(() => ({data: peachPublicKey})),
                 post: jest.fn()
             }
         }
@@ -42,12 +48,80 @@ describe("verify_we_can_build_daily_summaries", () => {
             axios: mockAxios
          };
          
-        eval(utcDateServiceData);
-        eval(browsingHistoryUploadServiceData);
+         const tweetNaclData = fs.readFileSync('../src/js/dist/nacl.min.js', 'utf8');
+         eval(tweetNaclData);
+         // Browserify helpfully puts our function on module.exports for us
+         let nacl = module.exports;
+         module.exports = {};
+ 
+         // Inject the browserfied tweetnacl.js helper library
+         const tweetNaclHelperData = fs.readFileSync('../src/js/tweetNaclHelper.js', 'utf8')
+ 
+         eval(tweetNaclHelperData);
 
-        // Return the µBlock object ready for the caller to use in it's tests
+         const tweetNaclUtilData = fs.readFileSync('../src/js/dist/nacl-util.min.js', 'utf8');
+         eval(tweetNaclUtilData);
+         // Browserify helpful puts our function on module.exports for us
+         nacl.util = module.exports;
+         module.exports = {};
+   
+         // Inject the browserfied tweetnacl-util.js helper library
+         const tweetNaclUtilHelperData = fs.readFileSync('../src/js/tweetNaclUtilHelper.js', 'utf8')
+   
+         eval(tweetNaclUtilHelperData);
+         eval(utcDateServiceData);
+         eval(browsingHistoryUploadServiceData);
+
+         const sessionKeysData = fs.readFileSync('../src/js/dist/sessionKeys.js', 'utf8');
+         eval(sessionKeysData);
+         // Browserify helpful puts our function on module.exports for us
+         let sessionKeys = module.exports;
+         module.exports = {};
+ 
+         // Run the scryptPromise module and inject the browserfied scryptsy library
+         const sessionKeysHelperData = fs.readFileSync('../src/js/sessionKeysHelper.js', 'utf8')
+ 
+         eval(sessionKeysHelperData);
+
+         // Return the µBlock object ready for the caller to use in it's tests
         return µBlock;
     };
+
+    function GenerateNaclKeysFromHashedPassword(userName, password) 
+    {
+        const sessionKeys = require('session-keys');
+        let promise = new Promise(function(resolve, reject) {
+            sessionKeys.generate(userName, password, function(err, keys) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            let result = {
+                naclSigningKeyPairBase64: keys.naclSigningKeyPairsBase64[0],
+                naclEncryptionKeyPairBase64: keys.naclEncryptionKeyPairsBase64[0]
+            };
+            resolve(result);
+            //return result;
+            // {
+            //   id: "0123456789abcdef",
+            //   byteKeys: [...],
+            //   hexKeys: [...],
+            //   naclEncryptionKeyPairs: [...],
+            //   naclEncryptionKeyPairsBase64: [...],
+            //   naclSigningKeyPairs: [...],
+            //   naclSigningKeyPairsBase64: [...],
+            // }
+        })
+    });
+    return promise;
+    }
+
+    async function GetSenderPublicKey() {
+        const { scrypt } = require('../services/scryptPromise');
+        let peachHashedKey = nodejsTweetNacl.util.encodeBase64(await scrypt("password".normalize('NFKC'), "PEACH", 32));
+        let senderKeys = await GenerateNaclKeysFromHashedPassword('mark@deliveryengine.net', peachHashedKey);
+        return { peachHashedKey, senderPublicKeyBase64: senderKeys.naclEncryptionKeyPairBase64.publicKey};
+    }
 
     it("test_if_no_browser_history_nothing_is_uploaded", async () => {
         let µBlock = initiseUploadEnvironment({});
@@ -72,6 +146,8 @@ describe("verify_we_can_build_daily_summaries", () => {
         const mockTime = require('jest-mock-now');
         mockTime(new Date('2021-06-10T01:00:00Z'));
 
+        let { peachHashedKey, senderPublicKeyBase64 } = await GetSenderPublicKey();
+
         let mockLocalStorage = {
             PEACHUPLOADSTARTDATE: (new Date('2021-06-08')).getTime(),
             ["PEACHUPLOAD2021-06-08"]: [
@@ -81,34 +157,60 @@ describe("verify_we_can_build_daily_summaries", () => {
             ["PEACHUPLOAD2021-06-09"]: [
                 { url: "https://www.businessinsider.com", at: (new Date('2021-06-09T23:00:01Z')).getTime()},
                 { url: "https://www.yahoo.com", at: (new Date('2021-06-09T23:00:02Z')).getTime()}
-            ]
+            ],
+            PEACHUSERNAME: 'mark@deliveryengine.net',
+            PEACHKEY: peachHashedKey
         };
 
-        let µBlock = initiseUploadEnvironment(mockLocalStorage);
+        let mockAxios = {
+            get: jest.fn(() => ({data: peachPublicKey})),
+            post: jest.fn()
+        }
+
+        let µBlock = initiseUploadEnvironment(mockLocalStorage, mockAxios);
 
         let task = µBlock.uploadBrowsingHistoryUpdates();
         expect(µBlock.uploadBrowsingHistoryLock).toBeTruthy();
         await task;
         expect(µBlock.uploadBrowsingHistoryLock).toBeFalsy();
 
-        expect(µBlock.axios.post).toHaveBeenCalledTimes(2);
+        expect(µBlock.axios.get).toHaveBeenCalledTimes(1);
+        let params = µBlock.axios.get.mock.calls;
+        expect(params[0][0].toString()).toBe('https://server.gopeach.app/api/peachPublicKey');
         
-        let params = µBlock.axios.post.mock.calls;
+        expect(µBlock.axios.post).toHaveBeenCalledTimes(2);
+        params = µBlock.axios.post.mock.calls;
 
         expect(params[0][0].toString()).toBe('https://server.gopeach.app/api/fileUpload');
         expect(params[1][0].toString()).toBe('https://server.gopeach.app/api/fileUpload');
-        let content1 = params[0][1].content;
-        content1 = JSON.parse(content1);
+        let uploaded = params[0][1];
+        expect(uploaded.senderPublicKey).toBe(senderPublicKeyBase64);
+        let decryptedContent = nodejsTweetNacl.box.open(
+            nodejsTweetNacl.util.decodeBase64(uploaded.encryptedContent),
+            nodejsTweetNacl.util.decodeBase64(uploaded.nonce),
+            nodejsTweetNacl.util.decodeBase64(uploaded.senderPublicKey),
+            peachKeys.secretKey
+        )
+        let content1 = JSON.parse(nodejsTweetNacl.util.encodeUTF8(decryptedContent));
         expect(content1.length).toBe(mockLocalStorage["PEACHUPLOAD2021-06-08"].length);
         for(let i = 0; i < mockLocalStorage["PEACHUPLOAD2021-06-08"].length ; i++) {
-            expect(dictEquals(content1[i], mockLocalStorage["PEACHUPLOAD2021-06-08"][i])).toBe(true);
+            let content = content1[i];
+            expect(dictEquals(content, mockLocalStorage["PEACHUPLOAD2021-06-08"][i])).toBe(true);
         }
 
-        let content2 = params[1][1].content;
-        content2 = JSON.parse(content2);
-        expect(content2.length).toBe(mockLocalStorage["PEACHUPLOAD2021-06-09"].length);
+        uploaded = params[1][1];
+        expect(uploaded.senderPublicKey).toBe(senderPublicKeyBase64);
+        decryptedContent = nodejsTweetNacl.box.open(
+            nodejsTweetNacl.util.decodeBase64(uploaded.encryptedContent),
+            nodejsTweetNacl.util.decodeBase64(uploaded.nonce),
+            nodejsTweetNacl.util.decodeBase64(uploaded.senderPublicKey),
+            peachKeys.secretKey
+        )
+        let content2 = JSON.parse(nodejsTweetNacl.util.encodeUTF8(decryptedContent));
+        expect(content1.length).toBe(mockLocalStorage["PEACHUPLOAD2021-06-09"].length);
         for(let i = 0; i < mockLocalStorage["PEACHUPLOAD2021-06-09"].length ; i++) {
-            expect(dictEquals(content2[i], mockLocalStorage["PEACHUPLOAD2021-06-09"][i])).toBe(true);
+            let content = content2[i];
+            expect(dictEquals(content, mockLocalStorage["PEACHUPLOAD2021-06-09"][i])).toBe(true);
         }
 
         expect(mockLocalStorage['PEACHUPLOADSTARTDATE']).toBe(µBlock.getStartOfUtcDay(new Date(Date.now())).getTime())
@@ -118,6 +220,8 @@ describe("verify_we_can_build_daily_summaries", () => {
         const mockTime = require('jest-mock-now');
         mockTime(new Date('2021-06-10T01:00:00Z'));
 
+        let { peachHashedKey, senderPublicKeyBase64 } = await GetSenderPublicKey();
+
         let mockLocalStorage = {
             PEACHUPLOADSTARTDATE: (new Date('2021-06-08')).getTime(),
             ["PEACHUPLOAD2021-06-08"]: [
@@ -127,12 +231,15 @@ describe("verify_we_can_build_daily_summaries", () => {
             ["PEACHUPLOAD2021-06-09"]: [
                 { url: "https://www.businessinsider.com", at: (new Date('2021-06-09T23:00:01Z')).getTime()},
                 { url: "https://www.yahoo.com", at: (new Date('2021-06-09T23:00:02Z')).getTime()}
-            ]
+            ],
+            PEACHUSERNAME: 'mark@deliveryengine.net',
+            PEACHKEY: peachHashedKey
         };
 
         let callCount = 0;
         let wasThrown = false;
         let mockAxios = {
+            get: jest.fn(() => ({data: peachPublicKey})),
             post: jest.fn(() => {
                 if (callCount > 0) {
                     wasThrown = true;
@@ -155,18 +262,35 @@ describe("verify_we_can_build_daily_summaries", () => {
 
         expect(params[0][0].toString()).toBe('https://server.gopeach.app/api/fileUpload');
         expect(params[1][0].toString()).toBe('https://server.gopeach.app/api/fileUpload');
-        let content1 = params[0][1].content;
-        content1 = JSON.parse(content1);
+
+        let uploaded = params[0][1];
+        expect(uploaded.senderPublicKey).toBe(senderPublicKeyBase64);
+        let decryptedContent = nodejsTweetNacl.box.open(
+            nodejsTweetNacl.util.decodeBase64(uploaded.encryptedContent),
+            nodejsTweetNacl.util.decodeBase64(uploaded.nonce),
+            nodejsTweetNacl.util.decodeBase64(uploaded.senderPublicKey),
+            peachKeys.secretKey
+        )
+        let content1 = JSON.parse(nodejsTweetNacl.util.encodeUTF8(decryptedContent));
         expect(content1.length).toBe(mockLocalStorage["PEACHUPLOAD2021-06-08"].length);
         for(let i = 0; i < mockLocalStorage["PEACHUPLOAD2021-06-08"].length ; i++) {
-            expect(dictEquals(content1[i], mockLocalStorage["PEACHUPLOAD2021-06-08"][i])).toBe(true);
+            let content = content1[i];
+            expect(dictEquals(content, mockLocalStorage["PEACHUPLOAD2021-06-08"][i])).toBe(true);
         }
 
-        let content2 = params[1][1].content;
-        content2 = JSON.parse(content2);
-        expect(content2.length).toBe(mockLocalStorage["PEACHUPLOAD2021-06-09"].length);
+        uploaded = params[1][1];
+        expect(uploaded.senderPublicKey).toBe(senderPublicKeyBase64);
+        decryptedContent = nodejsTweetNacl.box.open(
+            nodejsTweetNacl.util.decodeBase64(uploaded.encryptedContent),
+            nodejsTweetNacl.util.decodeBase64(uploaded.nonce),
+            nodejsTweetNacl.util.decodeBase64(uploaded.senderPublicKey),
+            peachKeys.secretKey
+        )
+        let content2 = JSON.parse(nodejsTweetNacl.util.encodeUTF8(decryptedContent));
+        expect(content1.length).toBe(mockLocalStorage["PEACHUPLOAD2021-06-09"].length);
         for(let i = 0; i < mockLocalStorage["PEACHUPLOAD2021-06-09"].length ; i++) {
-            expect(dictEquals(content2[i], mockLocalStorage["PEACHUPLOAD2021-06-09"][i])).toBe(true);
+            let content = content2[i];
+            expect(dictEquals(content, mockLocalStorage["PEACHUPLOAD2021-06-09"][i])).toBe(true);
         }
 
         expect(wasThrown).toBe(true);
